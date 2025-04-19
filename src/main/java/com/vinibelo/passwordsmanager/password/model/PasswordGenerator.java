f2b391711df1e04b1efd373218a11d9ceb9e6343
ViniBelo <vinialvesbelo@outlook.com>
2025-04-19 11:49:43 -0300

    Coded password generator to assert test password size

--- src/main/java/com/vinibelo/passwordsmanager/password/model/PasswordGenerator.java
+++ src/main/java/com/vinibelo/passwordsmanager/password/model/PasswordGenerator.java
@@ -1,7 +1,15 @@
 package com.vinibelo.passwordsmanager.password.model;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class PasswordGenerator {
     String generatePassword() {
-        return "";
+        StringBuilder password = new StringBuilder();
+        Random random = new Random();
+        IntStream.range(0, 20).forEach(i -> {
+            password.append(random.nextInt(0, 9));
+        });
+        return password.toString();
     }
 }
