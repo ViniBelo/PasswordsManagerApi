351250cf385a1322f19bf06dcf56d0d9ca48581e
ViniBelo <vinialvesbelo@outlook.com>
2025-04-18 23:35:54 -0300

    Initial commit

---
+++ src/main/java/com/vinibelo/passwordsmanager/PasswordsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.vinibelo.passwordsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordsManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordsManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinibelo/passwordsmanager/PasswordsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinibelo.passwordsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordsManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
