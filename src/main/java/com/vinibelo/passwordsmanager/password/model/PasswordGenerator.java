97c9d7b92bd09383ca5b271e0283cd1c46e108dd
ViniBelo <vinialvesbelo@outlook.com>
2025-04-22 19:24:05 -0300

    Generating passwords with both lower case characters and integers

--- src/main/java/com/vinibelo/passwordsmanager/password/model/PasswordGenerator.java
+++ src/main/java/com/vinibelo/passwordsmanager/password/model/PasswordGenerator.java
@@ -1,15 +1,30 @@
 package com.vinibelo.passwordsmanager.password.model;
 
+import java.util.Collections;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PasswordGenerator {
+    Random random = new Random();
+
     String generatePassword() {
         StringBuilder password = new StringBuilder();
-        Random random = new Random();
+        int firstDivision = random.nextInt(1, 19);
         IntStream.range(0, 20).forEach(i -> {
-            password.append(random.nextInt(0, 9));
+            if (i < firstDivision) {
+                int randomNumber = random.nextInt(0, 9);
+                password.append(randomNumber);
+            } else {
+                char randomLowerCaseChar = (char) random.nextInt(97, 122);
+                password.append(randomLowerCaseChar);
+            }
         });
+        Collections.shuffle(password
+                .toString()
+                .chars()
+                .boxed()
+                .collect(Collectors.toList()));
         return password.toString();
     }
 }
