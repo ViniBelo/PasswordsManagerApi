268572a194c60a020224485767d5f4ee152cf3d6
ViniBelo <vinialvesbelo@outlook.com>
2025-04-22 19:54:26 -0300

    Added upper cased character to the password

--- src/main/java/com/vinibelo/passwordsmanager/password/model/PasswordGenerator.java
+++ src/main/java/com/vinibelo/passwordsmanager/password/model/PasswordGenerator.java
@@ -1,6 +1,7 @@
 package com.vinibelo.passwordsmanager.password.model;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,21 +11,31 @@ public class PasswordGenerator {
 
     String generatePassword() {
         StringBuilder password = new StringBuilder();
-        int firstDivision = random.nextInt(1, 19);
-        IntStream.range(0, 20).forEach(i -> {
-            if (i < firstDivision) {
-                int randomNumber = random.nextInt(0, 9);
-                password.append(randomNumber);
-            } else {
-                char randomLowerCaseChar = (char) random.nextInt(97, 122);
-                password.append(randomLowerCaseChar);
-            }
+        int firstDivision = random.nextInt(1, 18);
+        int secondDivision = random.nextInt(1, (20 - firstDivision) - 1);
+        int thirdDivision = 20 - secondDivision - firstDivision;
+        IntStream.range(0, firstDivision).forEach(i -> {
+            int randomNumber = random.nextInt(0, 9);
+            password.append(randomNumber);
         });
-        Collections.shuffle(password
-                .toString()
-                .chars()
-                .boxed()
-                .collect(Collectors.toList()));
-        return password.toString();
+        IntStream.range(0, secondDivision).forEach(i -> {
+            char randomLowerCaseChar = (char) random.nextInt(97, 122);
+            password.append(randomLowerCaseChar);
+        });
+        IntStream.range(0, thirdDivision).forEach(i -> {
+            char randomUpperCaseChar = (char) random.nextInt(65, 90);
+            password.append(randomUpperCaseChar);
+        });
+        return shuffle(password.toString());
+    }
+
+    private String shuffle(String password) {
+        List<Character> characters = new java.util.ArrayList<>(
+                password.chars()
+                        .mapToObj(c -> (char) c)
+                        .toList()
+        );
+        Collections.shuffle(characters);
+        return characters.stream().map(Object::toString).collect(Collectors.joining());
     }
 }
