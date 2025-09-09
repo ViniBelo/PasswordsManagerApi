33ae16cbc113e74426a628f41aaea4cd1e06f3fa
ViniBelo <vinialvesbelo@outlook.com>
2025-09-09 19:24:45 -0300

    Using Authentication to pass username as params on PasswordService to create a new password

--- src/main/java/com/vinibelo/passwordsmanager/api/controller/password/PasswordsController.java
+++ src/main/java/com/vinibelo/passwordsmanager/api/controller/password/PasswordsController.java
@@ -2,10 +2,14 @@ package com.vinibelo.passwordsmanager.api.controller.password;
 
 import com.vinibelo.passwordsmanager.api.controller.password.dto.password.CreatePasswordRequestDto;
 import com.vinibelo.passwordsmanager.api.controller.password.dto.password.CreatePasswordResponseDto;
+import com.vinibelo.passwordsmanager.api.controller.password.exception.UnautorizedException;
 import com.vinibelo.passwordsmanager.password.entity.Password;
 import com.vinibelo.passwordsmanager.api.service.password.PasswordService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -22,14 +26,20 @@ public class PasswordsController {
     @PostMapping()
     public ResponseEntity<CreatePasswordResponseDto> generatePassword(
             @RequestBody CreatePasswordRequestDto createPasswordRequestDto,
-            HttpServletRequest request
+            Authentication authentication
     ) {
-        String token = request.getHeader("Authorization");
-        Password password = passwordService.newPassword(token, createPasswordRequestDto.platformId());
-        String uri = "/passwords/" + password.getId();
-        CreatePasswordResponseDto responseDto = new CreatePasswordResponseDto(
-                password.getPlatform().getNick(),
-                password.getPassword());
-        return ResponseEntity.created(URI.create(uri)).body(responseDto);
+        try {
+            var username = authentication.getName();
+            Password password = passwordService.newPassword(username, createPasswordRequestDto.platformId());
+            String uri = "/passwords/" + password.getId();
+            CreatePasswordResponseDto responseDto = new CreatePasswordResponseDto(
+                    password.getPlatform().getNick(),
+                    password.getPassword());
+            return ResponseEntity.created(URI.create(uri)).body(responseDto);
+        } catch (ChangeSetPersister.NotFoundException exception) {
+            return ResponseEntity.notFound().build();
+        } catch (UnautorizedException exception) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
--- src/main/java/com/vinibelo/passwordsmanager/api/service/password/PasswordService.java
+++ src/main/java/com/vinibelo/passwordsmanager/api/service/password/PasswordService.java
@@ -1,11 +1,13 @@
 package com.vinibelo.passwordsmanager.api.service.password;
 
+import com.vinibelo.passwordsmanager.api.controller.password.exception.UnautorizedException;
 import com.vinibelo.passwordsmanager.api.service.utils.TokenManipulator;
 import com.vinibelo.passwordsmanager.password.domain.PasswordGenerator;
 import com.vinibelo.passwordsmanager.password.entity.Password;
 import com.vinibelo.passwordsmanager.password.entity.Platform;
 import com.vinibelo.passwordsmanager.password.repository.PasswordRepository;
 import com.vinibelo.passwordsmanager.password.repository.PlatformRepository;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
@@ -15,19 +17,17 @@ import java.util.UUID;
 public class PasswordService {
     private final PasswordRepository passwordRepository;
     private final PlatformRepository platformRepository;
-    private final TokenManipulator tokenManipulator;
 
-    public PasswordService(PasswordRepository passwordRepository, PlatformRepository platformRepository, TokenManipulator tokenManipulator) {
+    public PasswordService(PasswordRepository passwordRepository, PlatformRepository platformRepository) {
         this.passwordRepository = passwordRepository;
         this.platformRepository = platformRepository;
-        this.tokenManipulator = tokenManipulator;
     }
 
-    public Password newPassword(String token, UUID platformId) {
-        Jwt decodedToken = tokenManipulator.getUserId(token);
-        Platform platform = platformRepository.findById(platformId).orElseThrow(RuntimeException::new);
-        if (!platform.getUser().getUsername().equals(decodedToken.getSubject()))
-            throw new RuntimeException("You don't have permission to access this platform");
+    public Password newPassword(String username, UUID platformId) throws ChangeSetPersister.NotFoundException {
+        Platform platform = platformRepository.findById(platformId)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new);
+        if (!platform.getUser().getUsername().equals(username))
+            throw new UnautorizedException("You don't have permission to access this platform");
         PasswordGenerator passwordGenerator = new PasswordGenerator();
         Password newPassword = new Password();
         newPassword.setPassword(passwordGenerator.generatePassword());
